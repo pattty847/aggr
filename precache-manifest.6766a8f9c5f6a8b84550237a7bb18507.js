5fc8615bb89767e1e68bf7f25ba4d3cd6617011b
Tucsky <tucsky@pm.me>
2022-08-14 06:22:36 +0200

    gh-pages

---
+++ precache-manifest.6766a8f9c5f6a8b84550237a7bb18507.js
@@ -0,0 +1,243 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52840f25079672cb0e4ae495a20f405",
+    "url": "/aggr/android-chrome-192x192.png"
+  },
+  {
+    "revision": "66da64cd54beba64b10e7f5c2e3d12e5",
+    "url": "/aggr/android-chrome-512x512.png"
+  },
+  {
+    "revision": "583704002e6c9b875237b890b5609631",
+    "url": "/aggr/apple-touch-icon.png"
+  },
+  {
+    "url": "/aggr/ba8cf03bbcebd4b15c459c565cd62e4b.svg"
+  },
+  {
+    "revision": "f79df19f54d7cc144e61c57ffe151084",
+    "url": "/aggr/browserconfig.xml"
+  },
+  {
+    "url": "/aggr/c82e309c4720a6d17045aa34a183e772.svg"
+  },
+  {
+    "revision": "d7f361600e75d7653e19",
+    "url": "/aggr/css/app.5f891957.css"
+  },
+  {
+    "revision": "6ef1742d188d55ee6203",
+    "url": "/aggr/css/chunk-107476e4.6dc9840e.css"
+  },
+  {
+    "revision": "2e248a0df7969702b204",
+    "url": "/aggr/css/chunk-1456abbc.cfb431e9.css"
+  },
+  {
+    "revision": "5dffa804dcacddb445b1",
+    "url": "/aggr/css/chunk-187d1773.058181ef.css"
+  },
+  {
+    "revision": "4055efd21bfeae1c9f2c",
+    "url": "/aggr/css/chunk-2bd9cf2c.7e8ff99a.css"
+  },
+  {
+    "revision": "c6666729ea928f37d975",
+    "url": "/aggr/css/chunk-2ce686f6.8dbd7c45.css"
+  },
+  {
+    "revision": "ba493dcac1ce0802eb5d",
+    "url": "/aggr/css/chunk-35847707.de824d83.css"
+  },
+  {
+    "revision": "b4f988195cdcc33d226d",
+    "url": "/aggr/css/chunk-35bede6e.903252b6.css"
+  },
+  {
+    "revision": "1439adc543dafbddadd1",
+    "url": "/aggr/css/chunk-48d3df3e.dbf034c7.css"
+  },
+  {
+    "revision": "80f2e1426794045c88e3",
+    "url": "/aggr/css/chunk-4cbfe090.40fb51df.css"
+  },
+  {
+    "revision": "38e63ea89cd3670b1b20",
+    "url": "/aggr/css/chunk-6aa087d4.791dd542.css"
+  },
+  {
+    "revision": "c4cff1d55280aa25853d",
+    "url": "/aggr/css/chunk-7845aa3c.7dc42311.css"
+  },
+  {
+    "url": "/aggr/de162994812461e4b01398865ad21372.svg"
+  },
+  {
+    "revision": "a04a99211da974612eab08a55a6ec5e5",
+    "url": "/aggr/favicon-16x16.png"
+  },
+  {
+    "revision": "8ab615f338ec790358c2d260b047daee",
+    "url": "/aggr/favicon-32x32.png"
+  },
+  {
+    "revision": "71bb69469e2abf699358d9e570c65c87",
+    "url": "/aggr/fonts/Barlow-Medium.71bb6946.woff2"
+  },
+  {
+    "revision": "a85515bc4b39e4b80ccc302dce4de6e0",
+    "url": "/aggr/fonts/Barlow-Medium.a85515bc.woff"
+  },
+  {
+    "revision": "58589bb037613f773367a9e7870f1e1f",
+    "url": "/aggr/fonts/Barlow-Regular.58589bb0.woff"
+  },
+  {
+    "revision": "61d84cc5a6a32dbfa4d9cf0c46990b3f",
+    "url": "/aggr/fonts/Barlow-Regular.61d84cc5.woff2"
+  },
+  {
+    "revision": "6889c62cd3a1a8737dff032c25a2ca09",
+    "url": "/aggr/fonts/Barlow-SemiBold.6889c62c.woff2"
+  },
+  {
+    "revision": "e969585cfdd0265857d714408a137e3e",
+    "url": "/aggr/fonts/Barlow-SemiBold.e969585c.woff"
+  },
+  {
+    "revision": "105a79c902ff68010f9bcbf8b4b99a98",
+    "url": "/aggr/fonts/icon.105a79c9.woff"
+  },
+  {
+    "revision": "a44d5b7ba4750b41d19571a099b5ada7",
+    "url": "/aggr/fonts/icon.a44d5b7b.ttf"
+  },
+  {
+    "revision": "adb587dba0be74745fc43f912e0d3fe2",
+    "url": "/aggr/index.html"
+  },
+  {
+    "revision": "4c5abb8519e990a59d3cf9c115ba2a86",
+    "url": "/aggr/js/aggregator.5ba741f4.worker.js"
+  },
+  {
+    "revision": "d7f361600e75d7653e19",
+    "url": "/aggr/js/app.e313766d.js"
+  },
+  {
+    "revision": "ee2576590da828f5726c",
+    "url": "/aggr/js/chunk-00f8fb9c.858693fb.js"
+  },
+  {
+    "revision": "079d08e879d84ae52a9e",
+    "url": "/aggr/js/chunk-10024d20.8ebea174.js"
+  },
+  {
+    "revision": "6ef1742d188d55ee6203",
+    "url": "/aggr/js/chunk-107476e4.48609679.js"
+  },
+  {
+    "revision": "2e248a0df7969702b204",
+    "url": "/aggr/js/chunk-1456abbc.b5f0dd0a.js"
+  },
+  {
+    "revision": "5dffa804dcacddb445b1",
+    "url": "/aggr/js/chunk-187d1773.22e7e72d.js"
+  },
+  {
+    "revision": "4055efd21bfeae1c9f2c",
+    "url": "/aggr/js/chunk-2bd9cf2c.00630cce.js"
+  },
+  {
+    "revision": "c6666729ea928f37d975",
+    "url": "/aggr/js/chunk-2ce686f6.a958cc7b.js"
+  },
+  {
+    "revision": "99e6f5a3447c88dbb657",
+    "url": "/aggr/js/chunk-2d0cc844.534b1991.js"
+  },
+  {
+    "revision": "252d495cdd3e345ab66f",
+    "url": "/aggr/js/chunk-2f4fd947.822e9851.js"
+  },
+  {
+    "revision": "ba493dcac1ce0802eb5d",
+    "url": "/aggr/js/chunk-35847707.32238db9.js"
+  },
+  {
+    "revision": "b4f988195cdcc33d226d",
+    "url": "/aggr/js/chunk-35bede6e.4e571d23.js"
+  },
+  {
+    "revision": "1439adc543dafbddadd1",
+    "url": "/aggr/js/chunk-48d3df3e.eaf17294.js"
+  },
+  {
+    "revision": "80f2e1426794045c88e3",
+    "url": "/aggr/js/chunk-4cbfe090.d9ddb1f9.js"
+  },
+  {
+    "revision": "150837678a0d36f93cb0",
+    "url": "/aggr/js/chunk-653050d6.c06e8cde.js"
+  },
+  {
+    "revision": "38e63ea89cd3670b1b20",
+    "url": "/aggr/js/chunk-6aa087d4.78dcda5f.js"
+  },
+  {
+    "revision": "30763e485f294fd3ba9e",
+    "url": "/aggr/js/chunk-6edad2a6.376e5ac0.js"
+  },
+  {
+    "revision": "c4cff1d55280aa25853d",
+    "url": "/aggr/js/chunk-7845aa3c.810bf325.js"
+  },
+  {
+    "revision": "0e1cc4f1bcca1ef1f56e",
+    "url": "/aggr/js/chunk-85145916.eacab412.js"
+  },
+  {
+    "revision": "61cc52635c3322d6b463",
+    "url": "/aggr/js/chunk-d2fbbe60.7f14c283.js"
+  },
+  {
+    "revision": "e3690d55f1f1e9f6bb6c",
+    "url": "/aggr/js/chunk-vendors.52618db9.js"
+  },
+  {
+    "revision": "6c554171d67a46482cf8e623a368e579",
+    "url": "/aggr/manifest.json"
+  },
+  {
+    "revision": "2d1bc7e6b11d8505fc5c04ae90d27371",
+    "url": "/aggr/mstile-144x144.png"
+  },
+  {
+    "revision": "36752d2829fff48d3b84cd2e9bac3b83",
+    "url": "/aggr/mstile-150x150.png"
+  },
+  {
+    "revision": "d723637642ba8f50a191e7a5bd6428f7",
+    "url": "/aggr/mstile-310x150.png"
+  },
+  {
+    "revision": "30107930797b320210e349ed601a9dd9",
+    "url": "/aggr/mstile-310x310.png"
+  },
+  {
+    "revision": "57fe6e3c59e6887fab1e7ab8bff2649a",
+    "url": "/aggr/mstile-70x70.png"
+  },
+  {
+    "revision": "f12f624acf3383b3ecc01ae877288235",
+    "url": "/aggr/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "4221b59e6b7fcb61fa04c2731bf21a31",
+    "url": "/aggr/site.webmanifest"
+  },
+  {
+    "revision": "8c80c4bc967385ab4d62abdd5eb4881e",
+    "url": "/aggr/sw.js"
+  }
+]);
